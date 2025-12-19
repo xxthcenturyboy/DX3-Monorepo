04fbf69e9462822ef1c023663c3c09e960828310
Dan Underwood <dunderwood73@gmail.com>
2025-12-19 12:19:58 -0800

    Updated uuid regex

--- packages/shared/utils/src/lib/regex-patterns.ts
+++ packages/shared/utils/src/lib/regex-patterns.ts
@@ -14,6 +14,6 @@ export const regexPostgresUrl =
   /^(?:([^:/?#\s]+):\/{2})?(?:([^@/?#\s]+)@)?([^/?#\s]+)?(?:\/([^?#\s]*))?(?:[?]([^#\s]+))?\S*$/
 
 export const regexUuid =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
 
 export const regexMatchNumberGroups = /\b\d+\b/g
