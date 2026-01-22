3a36065f8bb0e7708b7d10a5974d01150a753525
Dan Underwood <dunderwood73@gmail.com>
2026-01-22 11:00:03 -0800

    Updated consts.spec

--- packages/api/api-app/src/config/config-api.consts.spec.ts
+++ packages/api/api-app/src/config/config-api.consts.spec.ts
@@ -2,6 +2,8 @@ import {
   API_APP_NAME,
   API_ERROR,
   API_ROOT,
+  AWS_ACCESS_KEY,
+  AWS_SECRET_ACCESS_KEY,
   // APP_PREFIX,
   CRYPT_KEY,
   ERROR_MSG_API,
@@ -9,9 +11,7 @@ import {
   JWT_REFRESH_SECRET,
   OTP_SALT,
   POSTGRES_URI,
-  S3_ACCESS_KEY_ID,
   S3_APP_BUCKET_NAME,
-  S3_SECRET_ACCESS_KEY,
   SENDGRID_API_KEY,
   SENDGRID_URL,
   UPLOAD_MAX_FILE_SIZE,
@@ -22,15 +22,15 @@ describe('Config API Constants', () => {
     expect(API_APP_NAME).toBeDefined()
     expect(API_ERROR).toBeDefined()
     expect(API_ROOT).toBeDefined()
+    expect(AWS_ACCESS_KEY).toBeDefined()
+    expect(AWS_SECRET_ACCESS_KEY).toBeDefined()
     expect(CRYPT_KEY).toBeDefined()
     expect(ERROR_MSG_API).toBeDefined()
     expect(JWT_ACCESS_SECRET).toBeDefined()
     expect(JWT_REFRESH_SECRET).toBeDefined()
     expect(OTP_SALT).toBeDefined()
     expect(POSTGRES_URI).toBeDefined()
-    expect(S3_ACCESS_KEY_ID).toBeDefined()
     expect(S3_APP_BUCKET_NAME).toBeDefined()
-    expect(S3_SECRET_ACCESS_KEY).toBeDefined()
     expect(SENDGRID_API_KEY).toBeDefined()
     expect(SENDGRID_URL).toBeDefined()
     expect(UPLOAD_MAX_FILE_SIZE).toBeDefined()
